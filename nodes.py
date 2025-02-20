126626ac3f293d08a06b42f3056cbd17219c8fd6
Kyle Huang <iamkaikai@livemail.tw>
2025-02-20 11:12:52 +0800

    change default values

--- nodes.py
+++ nodes.py
@@ -6,31 +6,31 @@ class FloatPiecewiseRemap:
         return {
             "required": {
                 "value": ("FLOAT", {"default": 0.5}),
-                # Interval 1: e.g. map current 1 to 0.6 -> new 1 to 0.9
+                # Interval 1: e.g. map current 1 to 0.9 -> new 1 to 069
                 "cur_min_1": ("FLOAT", {"default": 1.0}),
                 "cur_max_1": ("FLOAT", {"default": 0.6}),
                 "new_min_1": ("FLOAT", {"default": 1.0}),
                 "new_max_1": ("FLOAT", {"default": 0.9}),
-                # Interval 2: e.g. map current 0.6 to 0.4 -> new 0.9 to 0.8
+                # Interval 2: e.g. map current 0.9 to 0.8 -> new 0.6 to 0.4
                 "cur_min_2": ("FLOAT", {"default": 0.6}),
                 "cur_max_2": ("FLOAT", {"default": 0.4}),
                 "new_min_2": ("FLOAT", {"default": 0.9}),
                 "new_max_2": ("FLOAT", {"default": 0.8}),
-                # Interval 3 e.g. map current 0.4 to 0.3 -> new 0.8 to 0.7
+                # Interval 3 e.g. map current 0.8 to 0.7 -> new 0.4 to 0.3
                 "cur_min_3": ("FLOAT", {"default": 0.4}),
                 "cur_max_3": ("FLOAT", {"default": 0.3}),
                 "new_min_3": ("FLOAT", {"default": 0.8}),
                 "new_max_3": ("FLOAT", {"default": 0.7}),
-                # Interval 4 e.g. map current 0.3 to 0.2 -> new 0.7 to 0.4
+                # Interval 4 e.g. map current 0.7 to 0.4 -> new 0.3 to 0.2
                 "cur_min_4": ("FLOAT", {"default": 0.3}),
                 "cur_max_4": ("FLOAT", {"default": 0.2}),
                 "new_min_4": ("FLOAT", {"default": 0.7}),
                 "new_max_4": ("FLOAT", {"default": 0.4}),
-                # Interval 5 e.g. map current 0.2 to 0.1 -> new 0.5 to 0.4
+                # Interval 5 e.g. map current 0.4 to 0.0 -> new 0.2 to 0.0
                 "cur_min_5": ("FLOAT", {"default": 0.2}),
-                "cur_max_5": ("FLOAT", {"default": 0.1}),
-                "new_min_5": ("FLOAT", {"default": 0.5}),
-                "new_max_5": ("FLOAT", {"default": 0.3}),
+                "cur_max_5": ("FLOAT", {"default": 0.0}),
+                "new_min_5": ("FLOAT", {"default": 0.4}),
+                "new_max_5": ("FLOAT", {"default": 0.0}),
             }
         }
 
